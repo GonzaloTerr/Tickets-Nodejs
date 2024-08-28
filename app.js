742d1c9ba1e8739d8a9601127db25404959e68f0
GonzaloTerr <makeappsolutions@gmail.com>
2024-08-28 10:43:44 -0300

    commit inicial

---
+++ app.js
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.get(`/hello`,(req, res)=>{
+    res.status(200).send(`Hello Gonzalo!`);
+});
+
+export default app;
